fee1650445d5192c9b92a89cb1d91f3a5740ae1e
AmarB <amar@b>
2020-04-11 20:39:45 -0400

    updates

--- graph_image_segmentation.py
+++ graph_image_segmentation.py
@@ -1,18 +1,24 @@
 import math
+import random
+from itertools import product, chain
+from functools import partial, reduce
+from sys import argv
+from multiprocessing import Pool
+import ntpath
+import heapq 
+
+# dependencies
 import networkx as nx
 import numpy as np
 from scipy import ndimage
 from imageio import imread, imwrite
-from sys import argv
-import math
-import random
 
 class Component:
     def __init__(self, rep_id, size):
         self.rep_id = rep_id
         self.size = size
         self.int = math.inf
-        self.color = self._rnd_color()
+        self.color = rnd_color()
 
     def __eq__(self, other):
         if other is None:
@@ -20,8 +26,8 @@ class Component:
         else: 
             return self.rep_id == other.rep_id
 
-    def _rnd_color(self):
-        return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    def __hash__(self):
+        return hash((self.rep_id, self.size, self.int))
 
 class DisjointSet:
     def __init__(self, id):
@@ -36,10 +42,10 @@ class DisjointSet:
             return False
         return self.id == other.id
 
-    def __ne__(self, other):
-        if type(self) != type(other):
-            return True
-        return self.id != other.id
+    # def __ne__(self, other):
+    #     if type(self) != type(other):
+    #         return True
+    #     return self.id != other.id
 
     def __hash__(self):
         return hash((self.id, self.rank))
@@ -57,11 +63,10 @@ class DisjointSet:
     def _link(self, other):
         if self.rank > other.rank:
             other._update_parent(self)
-        elif other.rank > self.rank:
-            self._update_parent(other)
         else:
-            self.rank = self.rank + 1
-            other._update_parent(self)
+            self._update_parent(other)
+            if self.rank == other.rank:
+                self.rank += 1
 
     def union(self, other):
         self.find_set()._link(other.find_set())
@@ -76,6 +81,61 @@ class DisjointSet:
             self._comp = None
             return node_set.get_component()
 
+# Tools
+ADJ = [(i,j) for i in (-1,0,1) for j in (-1,0,1) if not (i == j == 0)]
+RGB = ['r', 'g' 'b']
+
+def rnd_color():
+    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+def int_diff(pixel_a, pixel_b, im):
+    return math.sqrt(abs(im[pixel_a] - im[pixel_b]))
+
+def int_diff_color(pixel_a, pixel_b, im):
+    return math.sqrt(sum( math.pow(im[pixel_a+(i,)] - im[pixel_b+(i,)], 2) for i in range(3)))
+
+def get_edge(pos, im):
+    shape = im.shape
+    rows, cols = shape[0], shape[1]
+    adj_edges = []
+
+    for nbr in [(pos[0]+d[0], pos[1]+d[1]) for d in ADJ]:
+
+        if 0 <= nbr[0] < rows and 0 <= nbr[1] < cols:
+            if len(shape) == 2:
+                weight = {"intensity_diff": int_diff(pos, nbr, im)}
+            else:
+                weight = {"intensity_diff": int_diff_color(pos, nbr, im)}
+            adj_edges.append((pos, nbr, weight))
+
+    return adj_edges
+
+def get_image_graph(mode, sigma=0):
+    im = ndimage.gaussian_filter(imread(filepath,  pilmode=mode), sigma)
+    shape = im.shape
+    print("image shape:", im.shape)
+
+    G = nx.Graph()
+    f = partial(get_edge, im=im)
+    itr = product(range(shape[0]), range(shape[1]))
+    l = pool.map(f, itr)
+    G.add_edges_from(chain(*l))
+    return G
+
+def draw_segmentation(G, shape):
+    if (len(shape) == 3):
+        rows, cols, _ = shape
+    else:
+        rows, cols = shape 
+    im = np.zeros((rows,cols, 3)).astype(np.uint8)
+    for (r,c), rgb in nx.get_node_attributes(G, 'component').items():
+        im[r,c, 0] = rgb[0]
+        im[r,c, 1] = rgb[1]
+        im[r,c, 2] = rgb[2]
+    imwrite('z.jpg', im)
+
+
+# Segmentation from NX graph
 def make_set(id):
     return DisjointSet(id)
 
@@ -85,92 +145,61 @@ def min_internal_diff(n_i, n_j, k):
     mint = min(comp_value(c_i), comp_value(c_j))
     return mint
 
-def get_image_graph(filepath, sigma):
-    im = ndimage.gaussian_filter(imread(filepath,  pilmode='L'), sigma, output='int')
-
-    adj = [(i,j) for i in (-1,0,1) for j in (-1,0,1) if not (i == j == 0)]
-    # int_diff = lambda a, b: math.sqrt(
-    #     math.pow(im[a[0], a[1], 0] - im[b[0], b[1], 0],2 ) +
-    #     math.pow(im[a[0], a[1], 1] - im[b[0], b[1], 1],2 ) +
-    #     math.pow(im[a[0], a[1], 2] - im[b[0], b[1], 2],2 ),  
-    # )
-    int_diff = lambda a, b: math.sqrt(
-        abs(im[a[0], a[1]] - im[b[0], b[1]])
-    )
-    print(im.shape)
-    rows, cols = im.shape
-    G = nx.Graph()
-    for r in range(rows):
-        for c in range(cols):
-            for dr, dc in adj:
-                sr, sc = r + dr, c + dc
-                if 0 <= sr < rows and 0 <= sc < cols:
-                    _int = int_diff((r,c), (sr,sc))
-                    G.add_edge( (r,c), (sr, sc), weight=_int)
-    return G
-
-def draw_segmentation(G,shape):
-    if (len(shape) == 3):
-        rows, cols, _ = shape
-    else:
-        rows, cols = shape 
-    im = np.zeros((rows,cols, 3))
-    for (r,c), rgb in nx.get_node_attributes(G, 'component').items():
-        im[r,c, 0] = rgb[0]
-        im[r,c, 1] = rgb[1]
-        im[r,c, 2] = rgb[2]
-    imwrite('z.jpg', im)
-
-
-def segment(G, sigma=0.75, k=1): 
-    sorted_edges = sorted(G.edges(data='weight'), key=lambda e: e[2])
+def segment(G, k, weight_key):
+    sorted_edges = sorted(G.edges(data=weight_key), key=lambda e: e[2])
     forest = {n: make_set(n) for n in G.nodes()}
 
-    for id_i, id_j,w_q in sorted_edges:
+    for id_i, id_j, w_q in sorted_edges:
         n_i, n_j = forest[id_i], forest[id_j]
 
-        if (n_i.find_set() != n_j.find_set()) and w_q < min_internal_diff(n_i, n_j, k):
+        if (n_i.find_set() != n_j.find_set()) and w_q <= min_internal_diff(n_i, n_j, k):
             n_i.union(n_j)
             n_i.get_component().int = w_q
 
-    cs =  {k: v.get_component().color for k,v in forest.items()}
-    print( set(cs.values()) )
+    return {k: v.get_component().color for k,v in forest.items()}
+
+def segment_and_update_graph(G, k=1):
+    cs = segment(G, k, "intensity_diff")
     nx.set_node_attributes(G, cs, 'component')
-    return G
-
-def rnd_color():
-    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-
-# src: CLRS
 
 
-def test_graph():
-    G = nx.Graph()
-    G.add_edge(0, 1, weight=4)
-    G.add_edge(0, 7, weight=8)
-    G.add_edge(1, 7, weight=11)
-    G.add_edge(1, 2, weight=8)
-    G.add_edge(7, 8, weight=7)
-    G.add_edge(7, 6, weight=1)
+# def distance(data, query):
+#     return np.sqrt(np.square(data-query).sum(axis=1))
 
-    G.add_edge(2, 8, weight=2)
-    G.add_edge(8, 6, weight=6)
-    G.add_edge(2, 3, weight=7)
-    G.add_edge(2, 5, weight=4)
-    G.add_edge(6, 5, weight=2)
-    G.add_edge(3, 5, weight=14)
-    G.add_edge(3, 4, weight=9)
-    G.add_edge(5, 4, weight=10)
-    # G.add_edge(50, 51, weight=1000)
-    # G.add_edge(50, 2, weight=1000)
-    # G.add_edge(52, 51, weight=1003)
+# def knn(data, k):
+#     edges = []
+#     print("finding knn")
+#     for i in range(len(data)):
+#         distances = distance(data, data[i])
+        
+#         heap = [(d, tuple(data[j])) for j, d in enumerate(distances) if i != j]
+#         heapq.heapify(heap)     
+#         nearest_nbrs = [heapq.heappop(heap)[1] for _ in range(k)]
 
-    return G
+#     print('done')
+
+# def flatten_im_matrix(im):
+#     row, cols, color = im.shape
+#     coords = list(product(range(row), range(cols)))
+#     m = np.zeros( (len(coords),5))
+#     for i, (r,c) in enumerate(coords):
+#         m[i] = [r,c, im[r,c,0], im[r,c,1], im[r,c,2]]
+#     print(m)
+#     knn(m, 10)
+#     return m
 
 if __name__ == "__main__":
+    pool = Pool()
 
-    G = test_graph()
-    input_im = imread(argv[1])
-    G = segment(get_image_graph(argv[1], 0))
+    filepath = argv[1]
+    mode = "RGB" if argv[2] == "color" else "L"
+    
+    input_im = imread(filepath,  pilmode=mode)
+
+    G = get_image_graph(mode, sigma=0.8)
+
+    segment_and_update_graph(G, k=3000)
+
     draw_segmentation(G, input_im.shape)
-   # segment(G)
+
+    #print(flatten_im_matrix(input_im))
